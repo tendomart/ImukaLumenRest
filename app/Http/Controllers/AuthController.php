eefa8299f1bea7341714a5de45a6bfecfc627d97
tendomart <tmkmart@gmail.com>
2019-11-12 10:10:22 +0300

    Created Laravel-Lumen Imuka Backend

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,43 @@
+<?php
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use  App\User;
+
+class AuthController extends Controller
+{
+    /**
+     * Store a new user.
+     *
+     * @param  Request  $request
+     * @return Response
+     */
+    public function register(Request $request)
+    {
+        //validate incoming request 
+        $this->validate($request, [
+            'name' => 'required|string',
+            'password' => 'required|string',
+        ]);
+
+        try {
+
+            $user = new User;
+            $user->name = $request->input('name');
+            $plainPassword = $request->input('password');
+            $user->password = app('hash')->make($plainPassword);
+
+            $user->save();
+
+            //return successful response
+            return response()->json(['user' => $user, 'message' => 'CREATED'], 201);
+
+        } catch (\Exception $e) {
+            //return error message
+            return response()->json(['message' => 'User Registration Failed!'], 409);
+        }
+
+    }
+
+
+}
